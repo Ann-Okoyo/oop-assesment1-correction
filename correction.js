0e1549385700e4a29b2beef389b3482cc3f47d59
Muyale Okoyo <annmuyale@gmail.com>
2023-07-05 23:59:48 +0300

    first commit

---
+++ correction.js
@@ -0,0 +1,300 @@
+// class Ankaraa{
+//     constructor(temperature,mood ,occassion){
+//         this.temperature=temperature
+//         this.mood=mood
+//         this.occassion=occassion
+//     }
+//     getColor(){
+//     const temperature=20
+//     const mood=5
+//     if(this.temperature>=temperature&&this.mood>=mood){
+//         return "wear light and more patterned ankara"
+//     }
+//     else{
+//        return "wear dull and less patterened Ankara."
+//     }
+ 
+//     }
+    
+// }
+// let akara=new Ankaraa(26,6,"birthday")
+// console.log(akara.getColor())
+
+
+
+// // Scenario: Online Store Inventory Management
+// // You are building an inventory management system for an online store. 
+// // Create a program that allows the store owner to add, update, and 
+// // remove products from the inventory.
+// // class storeInventory{
+// //     constructor(products,quantity,price){
+// //         this.products=products
+// //         this.quantity=quantity
+// //         this.price=price
+
+// //     }
+// //     addProducts(){
+// //         storeInventory+=this.products
+// //     }
+// //     updateProducts(){
+
+// //     }
+// //     removeProducts(){
+// //         storeInventory-=this.products
+// //     }
+
+
+// // let storeinventory=new storeInventory()
+
+// class Ankara {
+//     constructor(temperature, mood, occassion) {
+//       this.temperature = temperature;
+//       this.mood = mood;
+//       this.occassion = occassion;
+//     }
+  
+//     getColor() {
+//       const temperatureThreshold = 2;
+//       const moodThreshold = 50;
+  
+//       if (this.temperature >= temperatureThreshold && this.mood >= moodThreshold) {
+//         return "Wear light and more patterned ankara.";
+//       } else {
+//         return "Wear dull and less patterned Ankara.";
+//       }
+//     }
+//   }
+  
+//   const ankara = new Ankara(26, 8, "wedding");
+//   console.log(ankara.getColor());
+
+
+//   class Product{
+//     constructor(price,quantity,product){
+//       this.product=[]
+//       this.pricet=price
+//       this.quantity=quantity
+//     }
+//   }
+//    class Inventory extends Product{
+//     constructor(product,quantity,price){
+//       super(price,product,quantity)
+//       this.product=[]
+//       this.quantity=quantity
+//       this.price=price
+//     }
+//     addProduct(){
+//       this.quantity +=this.quantity
+//       console.log(`${this.quantity} are the available ones `)
+//     }
+//     updateProduct(){
+//       this.product.append("fruits","vegetables","cashew nuts")
+//       console.log(`${this.product}`)
+
+//     }
+//     removeProduct(){
+//       this.product-=this.product
+//       console.log(`${this.product}`);
+//     }
+//    }
+// let product=new Product("ksh.600",70,"cereals")
+// product.addProduct(7);
+// product.removeProduct(15);
+// product.updateProduct("fruits","vegetables","cashew nuts")
+
+// class Products{
+//   constructor(price,quantity,name){
+//     this.name=name;
+//     this.price=price;
+//     this.quantity=quantity;
+//   }
+// }
+// class Inventory extends Products{
+//   constructor(price,quantity,name){
+//     super(price,quantity,name);
+//     this.products=[name];
+//   }
+//   addProduct(name,quantity){
+//     this.quantity+=quantity;
+//     console.log(`${quantity} ${name}added.Total quantity :${this.quantity}`)
+//   }
+//   updateProducts(products){
+// this.products.push(...products                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                              1Q``````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````QQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQ``````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````Q````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````1  QQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQ QQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQ11111111`                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                      11111111`)
+//   }
+// }
+
+// class Migration{
+//   constructor(waterPatterns,riverLevels,predatorLocation){
+//     this.waterPatterns=waterPatterns
+//     this.riverLevels=riverLevels
+//     this.predatorLocation=predatorLocation
+
+//   }
+//   predictingMovement(){
+//     const waterPatt=20
+//     const rivLevels=5
+
+//     if(waterPatt>=20 && rivLevels >=5){
+//       console.log("Migration will take place across the Serengeti-Mara.")
+//     }
+//     else{
+//       console.log("MIgration will not take place");
+//     }
+//   }
+//   predictingPredatorLocation(){
+//     const predatorLoc=6
+//     if(predatorLoc>=6){
+//       console.log("Migration is most likely to take place")
+//     }
+//     else{
+//       console.log("Migration will most likely fail")
+//     }
+//   }
+// }
+// let migration=new Migration(27,8,3);
+// migration.predictingMovement(12,5);
+// migration.predictingPredatorLocation(2);
+
+// class filmProject{
+//   constructor(members,shooting,budgeting){
+//     this.members=members
+//     this.shooting=shooting
+//     this.budgeting=budgeting
+
+//   }
+//   schedulingProject(){
+//     const shooting1=4
+//     if(this.shooting>=shooting1){
+//       console.log("Shooting will carry on.")
+//     }
+//     else{
+//       return("Shooting will not take place")
+//     }
+
+//   }
+//   budgetingProject(){
+//     const budget1=200000
+//     if(this.budgeting>=budget1){
+//       console.log(`The amount:${this.budget}is too high  to carry on with the project.`)
+//     }
+//     else{
+//       console.log("Handling of all the projects will carry on");
+//     }
+//   }
+//   cordinatngProject(){
+//     const members=10
+//     if(this.members>=members){
+//       console.log("Projects will go on one at a time")
+//     }
+//     else{
+//       console.log("No more projects till the next season");
+//     }
+//   }
+// }
+// let filmproject=new filmProject(6,400000,8)
+// console.log(filmproject.schedulingProject())
+// console.log(filmproject.budgetingProject())
+// console.log(filmproject.cordinatngProject())
+
+
+// const baobabTree ={
+//   currentSeason:'spring',
+//   fruits: [
+//     {
+//       type:'red fruit',
+//       power:'healing',
+//       effect:'restores health',
+//       season:'spring'
+//     },
+//     {
+//       type:'green fruit',
+//       power:'strength',
+//       effect:'increase  physical strength',
+//       season:'summer'
+//     },
+    
+//     {
+//       type:'purple fruit',
+//       power:'wisdom',
+//       effect:'grants knowledge and insight',
+//       season:'fall'
+    
+//     },
+//     {
+//       type:'yellow fruit',
+//       power:'luck',
+//       effect:'grants knowledge and insight',
+//       season:'fall'
+    
+//     },
+//     {
+//       type:'yellow fruit',
+//       power:'luck',
+//       effect:'brings good fortune and prosperity',
+//       season:'winter'
+//     }
+//   ]
+//   };
+//   function predictNextFruit(){
+//     const currentSeasonIndex =['spring','summer','fall','winter'].indexOf(baobabTree.currentSeason);
+//     const  nextSeasonIndex=(currentSeasonIndex+1)%4;
+
+//   }
+
+      
+    
+
+  
+// class Migration{
+//   constructor(weather,riverLevels,predatorLocation,animals){
+//     this.weather=weather
+//     this.riverLevels=riverLevels
+//     this.predatorLocation=predatorLocation
+//     this.animals=animals
+//   }
+//   predatorMigration(){
+    
+//   }
+// }
+class Ankara{
+  constructor(temperature,mood){
+    this.temperature=temperature
+    this.mood=mood
+
+  }
+  predictChanges(){
+    const temperature1=20
+    const mood1 =5
+
+    if(this.temperature>=temperature1 && this.mood>=mood1){
+      console.log("Wear a bright colored ,light Ankara");
+    }
+    else{
+      return ("Wear a dull colored,heavy Ankara")
+    }
+  }
+}
+const ankara =new Ankara(30,8)
+console.log(ankara.predictChanges())
+
+class Migration{
+  constructor(weatherPatterns,riverLevels,predatorLocation){
+    this.weatherPatterns=weatherPatterns
+    this.riverLevels=riverLevels
+    this.predatorLocation=predatorLocation
+  }
+  predictMovement(){
+    const weatherPatterns1=40
+    const riverLevels1=20
+
+    if(this.weatherPatterns>=weatherPatterns1 && this.riverLevels>=riverLevels1){
+      console.log("Animal will not migrate due to favourable weather patterns and riverlevels");
+    }
+    else{
+      console.log("Animals will migrate due to unfavourable conditions.");
+    }
+  }
+  predictLocation(){
+    
+  }
+}
